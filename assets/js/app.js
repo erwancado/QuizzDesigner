ce8f25618777632672329541a9223fb35a593908
ndesc <n.desc64@gmail.com>
2020-03-31 12:10:11 +0200

    update admin dash Board

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,4 +15,3 @@ require('bootstrap4-toggle');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
--- src/Controller/AdminDashBoardController.php
+++ src/Controller/AdminDashBoardController.php
@@ -5,20 +5,29 @@ namespace App\Controller;
 use App\Entity\Quiz;
 use App\Entity\Theme;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminDashBoardController extends AbstractController
 {
+
+
     /**
      * @Route("/admin/dashboard", name="admin_dash_board")
      */
-    public function index()
+    public function index(Request $request)
     {
-        
+
         $user = $this->getUser();
         $quizzes = $user->getQuizzes();
         $themes = $user->getThemes();
 
+        if($request->query->get('hide') != null)
+            $hide = $request->query->get('hide');
+        else
+            $hide = "theme";
 
         return $this->render('admin_dash_board/index.html.twig', [
             'controller_name' => 'AdminDashBoardController',
@@ -26,6 +35,7 @@ class AdminDashBoardController extends AbstractController
             'themes' => $themes,
             'quizTemplate' => 'admin_dash_board/quizTemplate.html.twig',
             'themeTemplate' => 'admin_dash_board/themeTemplate.html.twig',
+            'hide' => $hide,
         ]);
     }
 
@@ -47,7 +57,7 @@ class AdminDashBoardController extends AbstractController
         $entityManager->remove($theme);
         $entityManager->flush();
 
-        return $this->redirectToRoute('admin_dash_board');
+        return $this->redirectToRoute('admin_dash_board', ['hide' => 'quizz']);
 
     }
 
@@ -69,7 +79,7 @@ class AdminDashBoardController extends AbstractController
         $entityManager->remove($quiz);
         $entityManager->flush();
 
-        return $this->redirectToRoute('admin_dash_board');
+        return $this->redirectToRoute('admin_dash_board', ['hide' => 'theme']);
 
     }
 }
