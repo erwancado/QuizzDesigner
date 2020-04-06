74ec872f51fdd726d0159059eba815871b0e76a0
ndesc <n.desc64@gmail.com>
2020-04-06 18:22:10 +0200

    Add image field in DB for question Table

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -6,8 +6,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\QuestionRepository")
+ * @Vich\Uploadable()
  */
 class Question
 {
@@ -17,6 +21,18 @@ class Question
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @var File|null
+     * @Vich\UploadableField(mapping="questions", fileNameProperty="filename")
+     */
+    private $imageFile;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", length=255)
+     */
+    private $filename;
 
     /**
      * @ORM\Column(type="text")
@@ -165,4 +181,42 @@ class Question
 
         return $this;
     }
+
+    /**
+     * @return File|null
+     */
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @param File|null $imageFile
+     * @return Question
+     */
+    public function setImageFile(?File $imageFile): Question
+    {
+        $this->imageFile = $imageFile;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    /**
+     * @param string|null $filename
+     * @return Question
+     */
+    public function setFilename(?string $filename): Question
+    {
+        $this->filename = $filename;
+        return $this;
+    }
+
+
 }
