c6fd30cbe0789646946cd95c88fcaa6cb48c0cfe
ndesc <n.desc64@gmail.com>
2020-04-08 19:34:29 +0200

    upload quizz & question image OK

---
+++ src/Migrations/Version20200408171633.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200408171633 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE partie ADD question_in_progress_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE partie ADD CONSTRAINT FK_59B1F3D89B6B9E1 FOREIGN KEY (question_in_progress_id) REFERENCES question (id)');
+        $this->addSql('CREATE INDEX IDX_59B1F3D89B6B9E1 ON partie (question_in_progress_id)');
+        $this->addSql('ALTER TABLE question CHANGE filename filename VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE quiz CHANGE filename filename VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE partie DROP FOREIGN KEY FK_59B1F3D89B6B9E1');
+        $this->addSql('DROP INDEX IDX_59B1F3D89B6B9E1 ON partie');
+        $this->addSql('ALTER TABLE partie DROP question_in_progress_id');
+        $this->addSql('ALTER TABLE question CHANGE filename filename VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE quiz CHANGE filename filename VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
+    }
+}
